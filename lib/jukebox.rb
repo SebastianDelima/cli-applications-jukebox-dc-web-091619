c8c0037b56018b5410c01b358514a410347d0dd5
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-26 21:10:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -82,24 +82,5 @@ end
 #       return exit_jukebox
 #   end
 # end
-def run(songs)
-  while true do
-    puts "Please enter a command:"
-    response = gets.chomp
-    case response
-    when "exit"
-      exit_jukebox
-      break
-    when "play"
-      play(songs)
-    when "help"
-      help
-    when "list"
-      list(songs)
-    else
-      puts "Invalid entry"
-    end
-  end
-end
-run(songs)
+
 run(songs)
